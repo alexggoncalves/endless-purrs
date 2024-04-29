ce9fc6e402667ec7416f79c6ffee3ec1ec095348
Margarida Mendonça <149173078+Nara-Me@users.noreply.github.com>
2024-04-29 19:54:16 +0100

    EEEEEE

--- Assets/Scripts/CatBehaviour.cs
+++ Assets/Scripts/CatBehaviour.cs
@@ -1,4 +1,4 @@
-using System.Collections;
+/*using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -58,12 +58,86 @@ public class CatBehaviour : MonoBehaviour
 
             uiButton.transform.position = adjustedPointerPos + offset;*/
 
-        } else
+/*  } else
+  {
+      uiButton.gameObject.SetActive(false);
+  }
+
+
+  if (triggerActive && Input.GetKeyDown(KeyCode.E))
+  {
+      PlaySound(Meow[1].clip);
+      CatchCat();
+  }
+}
+
+private void PlaySound(AudioClip clip)
+{
+  GameObject soundObject = new GameObject("TemporarySoundObject");
+  AudioSource audioSource = soundObject.AddComponent<AudioSource>();
+
+  audioSource.clip = clip;
+  audioSource.Play();
+
+  Destroy(soundObject, clip.length);
+}
+
+public void CatchCat()
+{
+  // Save the ID of the object
+  objectId = gameObject.GetInstanceID();
+
+  print(objectId);
+  gameObject.SetActive(false);
+  uiButton.gameObject.SetActive(false);
+}
+}*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatBehaviour : MonoBehaviour
+{
+    private Camera cam;
+    [SerializeField] private bool triggerActive = false;
+    private int objectId;
+
+    private RectTransform uiButtonPrefab;
+
+    private AudioSource[] Meow;
+
+    private RectTransform currentUIButton;
+
+    private void Start()
+    {
+        cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>() as Camera;
+        Meow = gameObject.GetComponents<AudioSource>();
+
+        uiButtonPrefab = Resources.Load<RectTransform>("CatCatcher");
+        HideUIButton();
+    }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
         {
-            uiButton.gameObject.SetActive(false);
+            triggerActive = true;
+            ShowUIButton();
         }
+    }
 
+    public void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            triggerActive = false;
+            HideUIButton();
+        }
+    }
 
+    private void Update()
+    {
         if (triggerActive && Input.GetKeyDown(KeyCode.E))
         {
             PlaySound(Meow[1].clip);
@@ -89,6 +163,26 @@ public class CatBehaviour : MonoBehaviour
 
         print(objectId);
         gameObject.SetActive(false);
-        uiButton.gameObject.SetActive(false);
+        HideUIButton();
+    }
+
+    private void ShowUIButton()
+    {
+        if (currentUIButton == null && uiButtonPrefab != null)
+        {
+            Canvas canvas = GameObject.FindObjectOfType<Canvas>();
+
+            currentUIButton = Instantiate(uiButtonPrefab, Vector3.zero, Quaternion.identity) as RectTransform;
+            currentUIButton.SetParent(canvas.transform, false);
+        }
+    }
+
+    private void HideUIButton()
+    {
+        if (currentUIButton != null)
+        {
+            Destroy(currentUIButton.gameObject);
+            currentUIButton = null;
+        }
     }
 }
